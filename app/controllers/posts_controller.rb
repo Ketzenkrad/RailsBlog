d21e8779064bb7b89d33c758079fc1b723797c3f
Ketzenkrad <ketzenkrad@gmail.com>
2018-11-10 01:48:15 +0300

    Форма создания постов, запись в DB, перенаправления на созданный пост

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,29 @@
 class PostsController < ApplicationController
+
   def index
     #Функционал;
   end
+
+  def new
+    #Новые посты;
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    # render plain: params[:post].inspect
+    #Получаем параметры от формы с указанием типа;
+    @post = Post.new(post_params)
+    #сохраняем
+    @post.save
+    #Перенаправление
+    redirect_to @post
+  end
+
+  private def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
